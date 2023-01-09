5de497524c3233c64a93eb0e454bfbdd22398d37
geduramc <geduradev@gmail.com>
2023-01-09 15:16:24 -0500

    chore: refactor assets location

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import '../assets/main.css'
+import './assets/main.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
 
